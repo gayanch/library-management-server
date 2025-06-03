672aa628db72d40e1a17e646d23910e1dbb98f1a
Gayan Karunarathna <agchamara93@gmail.com>
2025-06-03 14:55:05 +0530

    Initial project setup

---
+++ src/main/java/com/github/gayanch/library/LibraryManagementServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.gayanch.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementServerApplication.class, args);
+	}
+
+}
